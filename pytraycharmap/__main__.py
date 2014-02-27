c0a9bbb4bd8ba2ece59332568513613e981e1a11
Dmitry V. Luciv <dluciv@dluciv.name>
2014-02-27 12:23:52 +0300

    Update __main__.py
    Typo fix

--- pytraycharmap/__main__.py
+++ pytraycharmap/__main__.py
@@ -6,7 +6,7 @@ import os
 
 if __name__ == "__main__":
     # Solution from pip built-in package.
-    # allows to run as both directory and moule with -m parameter
+    # Allows to run as both directory and module with -m parameter
     # Considered bad, but used oftenly.
     sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
     import pytraycharmap.charmap
