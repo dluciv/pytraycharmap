4f7101524c4cd33b20c21a80feebed255902189b
Dmitry V. Luciv <dluciv@dluciv.name>
2020-03-09 21:51:07 +0300

    Simpler for Windows with launcher

--- run.bat
+++ run.bat
@@ -1,18 +1,3 @@
-:: Full Windows power is needed to store command output in variable!
+set python=pyw -3
 
-:: We asume first python in path is python3 with PyQt5 installed
-set python=
-for /F "tokens=*" %%R in ('where pythonw.exe') do call :setpython %%R
-
-set PATH=%PATH%;%python%\..\Lib\site-packages\PyQt5
 start %python% %~dp0pytraycharmap %~dp0menu\charmap.yaml
-goto :eof
-
-:: In Windows, we need a "function" to set a variable,
-:: we can't just set it inside for loop body
-:setpython
-if "%python%"=="" set python=%1
-goto :eof
-
-:: or so if it is in sys.path
-:: start %python% -m pytraycharmap %~dp0menu\charmap.yaml
